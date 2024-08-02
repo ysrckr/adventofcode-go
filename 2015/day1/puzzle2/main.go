487132f5c8f010c6dc6329d8409697243fa3539b
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-02 22:38:59 +0700

    Add file reading and position tracking logic

--- 2015/day1/puzzle2/main.go
+++ 2015/day1/puzzle2/main.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	count := 0
+	position := 0
+
+	reader := bufio.NewReader(file)
+
+	for {
+		r, _, err := reader.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+			break
+		}
+
+		if count == -1 {
+			break
+		}
+
+		position++
+
+		switch string(r) {
+		case "(":
+			count += 1
+		case ")":
+			count -= 1
+		default:
+			continue
+		}
+	}
+
+	fmt.Println(position)
 
 }
