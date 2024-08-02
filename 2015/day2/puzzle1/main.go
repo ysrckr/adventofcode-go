26c6a4ffd5e2853797acebda7820e2409c84f17b
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-02 23:10:26 +0700

    Add box dimension parsing and paper calculation

--- 2015/day2/puzzle1/main.go
+++ 2015/day2/puzzle1/main.go
@@ -1,5 +1,87 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Box struct {
+	l uint64
+	w uint64
+	h uint64
+}
+
+func newBox(str string) *Box {
+	splitString := strings.Split(str, "x")
+	length, err := strconv.Atoi(splitString[0])
+	if err != nil {
+		return nil
+	}
+
+	width, err := strconv.Atoi(splitString[1])
+	if err != nil {
+		return nil
+	}
+
+	height, err := strconv.Atoi(splitString[2])
+	if err != nil {
+		return nil
+	}
+
+	return &Box{
+		l: uint64(length),
+		w: uint64(width),
+		h: uint64(height),
+	}
+}
+
+func (b *Box) totalSurfaceArea() uint64 {
+	return 2*b.l*b.w + 2*b.w*b.h + 2*b.h*b.l
+}
+
+func (b *Box) smallestSurfaceArea() uint64 {
+	return min(b.l*b.w, b.w*b.h, b.h*b.l)
+}
+
+func (b *Box) totalPaperNeeded() uint64 {
+	return b.totalSurfaceArea() + b.smallestSurfaceArea()
+}
+
 func main() {
-	
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	reader := bufio.NewReader(file)
+
+	var total uint64 = 0
+	count := 0
+
+	for {
+		r, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+			break
+		}
+		count++
+		box := newBox(string(r))
+		total += box.totalPaperNeeded()
+	}
+
+	fmt.Println(total)
 }
