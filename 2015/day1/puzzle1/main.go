7f2a84d47f6a356d7c6967cd12aab17f47ab93a6
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-02 13:04:12 +0700

    Add initial solution for 2015 Day 1 Puzzle 1

---
+++ 2015/day1/puzzle1/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	count := 0
+
+	reader := bufio.NewReader(file)
+
+	for {
+		if r, _, err := reader.ReadRune(); err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatalln(err)
+		} else {
+			switch string(r) {
+			case "(":
+				count += 1
+			case ")":
+				count -= 1
+			default:
+				continue
+			}
+		}
+
+	}
+
+	fmt.Println(count)
+
+}
