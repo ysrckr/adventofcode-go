89983e76a1c93a438eac4ca30d63809f96b566b8
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-03 23:48:02 +0700

    <commit message>Add input file and code to read and print lines

--- 2015/day5/puzzle1/main.go
+++ 2015/day5/puzzle1/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
+func main() {
+	file, err := os.Open("../input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	reader := bufio.NewReader(file)
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Println(string(line))
+	}
 }
