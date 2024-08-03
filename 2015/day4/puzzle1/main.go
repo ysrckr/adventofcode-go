942016623c88eb296503fa12faef87ea193778fe
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-03 23:38:59 +0700

    Add MD5 hash logic to find hash with five zeros

--- 2015/day4/puzzle1/main.go
+++ 2015/day4/puzzle1/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func main() {
-	
+
+	counter := 0
+
+	for {
+
+		hash := getMD5Hash("bgvyzdsv" + strconv.Itoa(counter))
+
+		if isStartsWithFiveZeros(hash) {
+			break
+		}
+
+		counter++
+	}
+
+	fmt.Println(counter)
+}
+
+func getMD5Hash(text string) string {
+	hash := md5.Sum([]byte(text))
+	return hex.EncodeToString(hash[:])
+}
+
+func isStartsWithFiveZeros(text string) bool {
+	return strings.HasPrefix(text, "00000")
 }
