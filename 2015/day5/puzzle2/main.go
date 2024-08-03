b63925c7680b7061d7050549f8834e3dc22e666f
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-03 23:48:30 +0700

    Add initial implementation for Day 5, Puzzle 2

---
+++ 2015/day5/puzzle2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("../input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	reader := bufio.NewReader(file)
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Println(string(line))
+	}
+}
