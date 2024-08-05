aede6c7e50a029c901febb1b6d2fcec6097c1763
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-05 17:54:39 +0700

    Add nice string counting logic

--- 2015/day5/puzzle1/main.go
+++ 2015/day5/puzzle1/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -16,15 +17,46 @@ func main() {
 
 	reader := bufio.NewReader(file)
 
+	niceStringCount := 0
+
 	for {
 		line, _, err := reader.ReadLine()
 		if err == io.EOF {
-			return
+			break
 		}
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		fmt.Println(string(line))
+		if strings.Contains(string(line), "ab") || strings.Contains(string(line), "cd") || strings.Contains(string(line), "pq") || strings.Contains(string(line), "xy") {
+			continue
+		}
+
+		vowelCount := 0
+		hasDouble := false
+
+		for i, c := range line {
+			if i > 0 {
+				if line[i-1] == c {
+					hasDouble = line[i-1] == c
+				}
+			}
+			switch string(c) {
+			case "a", "e", "i", "o", "u":
+				vowelCount++
+
+			default:
+				continue
+			}
+		}
+
+		if vowelCount >= 3 && hasDouble {
+			niceStringCount++
+		} else {
+			continue
+		}
+
 	}
+
+	fmt.Println(niceStringCount)
 }
