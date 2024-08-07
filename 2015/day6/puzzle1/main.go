43761ebdb6ab1eb49af956fd0dd76932954b5152
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-07 21:15:41 +0700

    Refactor light command processing logic

--- 2015/day6/puzzle1/main.go
+++ 2015/day6/puzzle1/main.go
@@ -21,6 +21,27 @@ type Command struct {
 	endingCoords   Coords
 }
 
+func (c *Command) switchLight(grid *[1000][1000]int) {
+	for i := c.beginingCoords.x; i <= c.endingCoords.x; i++ {
+		for j := c.beginingCoords.y; j <= c.endingCoords.y; j++ {
+			if c.action == "on" {
+				grid[i][j] = 1
+			}
+
+			if c.action == "off" {
+				grid[i][j] = 0
+			}
+
+			if c.action == "toggle" {
+
+				grid[i][j] ^= 1
+
+			}
+		}
+	}
+
+}
+
 func main() {
 
 	result1 := puzzle1()
@@ -45,57 +66,68 @@ func puzzle1() int {
 			log.Fatalln(err)
 		}
 
-		command := &Command{}
-
-		if strings.Split(string(line), " ")[0] == "toggle" {
-			command.action = "toggle"
-			command.beginingCoords.x, err = strconv.Atoi(strings.Split(strings.Split(string(line), " ")[1], ",")[0])
-			if err != nil {
-				continue
-			}
-			command.beginingCoords.y, err = strconv.Atoi(strings.Split(strings.Split(string(line), " ")[1], ",")[1])
-			if err != nil {
-				continue
-			}
-			command.endingCoords.x, err = strconv.Atoi(strings.Split(strings.Split(string(line), " ")[3], ",")[0])
-			if err != nil {
-				continue
-			}
-			command.endingCoords.y, err = strconv.Atoi(strings.Split(strings.Split(string(line), " ")[3], ",")[1])
-			if err != nil {
-				continue
-			}
-		} else {
-			command.action = strings.Split(string(line), " ")[1]
-			command.beginingCoords.x, err = strconv.Atoi(strings.Split(strings.Split(string(line), " ")[2], ",")[0])
-			if err != nil {
-				continue
-			}
-			command.beginingCoords.y, err = strconv.Atoi(strings.Split(strings.Split(string(line), " ")[2], ",")[1])
-			if err != nil {
-				continue
-			}
-			command.endingCoords.x, err = strconv.Atoi(strings.Split(strings.Split(string(line), " ")[4], ",")[0])
-			if err != nil {
-				continue
-			}
-			command.endingCoords.y, err = strconv.Atoi(strings.Split(strings.Split(string(line), " ")[4], ",")[1])
-			if err != nil {
-				continue
-			}
-
+		command, err := newCommand(string(line))
+		if err != nil {
+			continue
 		}
 
-		switchLight(command.beginingCoords.x, command.beginingCoords.y, command.endingCoords.x, command.endingCoords.y, &grid, command.action)
+		command.switchLight(&grid)
 
 	}
 
-	Count(&grid, &count)
+	incrementCount(&grid, &count)
 
 	return count
 }
 
-func Count(grid *[1000][1000]int, count *int) {
+func newCommand(line string) (*Command, error) {
+	command := &Command{}
+	var err error
+	if strings.Split(line, " ")[0] == "toggle" {
+		command.action = "toggle"
+		command.beginingCoords.x, err = strconv.Atoi(strings.Split(strings.Split(line, " ")[1], ",")[0])
+		if err != nil {
+			return nil, err
+		}
+		command.beginingCoords.y, err = strconv.Atoi(strings.Split(strings.Split(line, " ")[1], ",")[1])
+		if err != nil {
+			return nil, err
+		}
+		command.endingCoords.x, err = strconv.Atoi(strings.Split(strings.Split(line, " ")[3], ",")[0])
+		if err != nil {
+			return nil, err
+		}
+		command.endingCoords.y, err = strconv.Atoi(strings.Split(strings.Split(line, " ")[3], ",")[1])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if strings.Split(line, " ")[0] != "toggle" {
+		command.action = strings.Split(line, " ")[1]
+		command.beginingCoords.x, err = strconv.Atoi(strings.Split(strings.Split(line, " ")[2], ",")[0])
+		if err != nil {
+			return nil, err
+		}
+		command.beginingCoords.y, err = strconv.Atoi(strings.Split(strings.Split(line, " ")[2], ",")[1])
+		if err != nil {
+			return nil, err
+		}
+		command.endingCoords.x, err = strconv.Atoi(strings.Split(strings.Split(line, " ")[4], ",")[0])
+		if err != nil {
+			return nil, err
+		}
+		command.endingCoords.y, err = strconv.Atoi(strings.Split(strings.Split(line, " ")[4], ",")[1])
+		if err != nil {
+			return nil, err
+		}
+
+	}
+
+	return command, nil
+}
+
+func incrementCount(grid *[1000][1000]int, count *int) {
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid); j++ {
 			if grid[i][j] == 1 {
@@ -124,25 +156,4 @@ func openFile() *bufio.Reader {
 
 	reader := bufio.NewReader(file)
 	return reader
-}
-
-func switchLight(beginingX, beginingY, endingX, endingY int, grid *[1000][1000]int, action string) {
-	for i := beginingX; i <= endingX; i++ {
-		for j := beginingY; j <= endingY; j++ {
-			if action == "on" {
-				grid[i][j] = 1
-			}
-
-			if action == "off" {
-				grid[i][j] = 0
-			}
-
-			if action == "toggle" {
-
-				grid[i][j] ^= 1
-
-			}
-		}
-	}
-
 }
