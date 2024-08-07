e00acc748d9c3543cfd3b355672d5ba0d99a954a
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-07 16:03:02 +0700

    Add puzzle2 function and IsNice helper functions

--- 2015/day5/puzzle2/main.go
+++ 2015/day5/puzzle2/main.go
@@ -6,9 +6,18 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
+
+	result := puzzle2()
+
+	fmt.Println(result)
+
+}
+
+func puzzle2() int {
 	file, err := os.Open("../input.txt")
 	if err != nil {
 		log.Fatalln(err)
@@ -16,15 +25,47 @@ func main() {
 
 	reader := bufio.NewReader(file)
 
+	count := 0
+
 	for {
 		line, _, err := reader.ReadLine()
 		if err == io.EOF {
-			return
+			break
 		}
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		fmt.Println(line)
+		isNice := IsNice(string(line))
+
+		if isNice {
+			count++
+		}
+
 	}
+
+	return count
+}
+
+func IsNice(s string) bool {
+	return hasPair(s) && hasRepeatedWithOneBetween(s)
+}
+
+func hasPair(s string) bool {
+	for i := 0; i < len(s)-1; i++ {
+		pair := s[i : i+2]
+		if strings.Count(s, pair) > 1 {
+			return true
+		}
+	}
+	return false
+}
+
+func hasRepeatedWithOneBetween(s string) bool {
+	for i := 0; i < len(s)-2; i++ {
+		if s[i] == s[i+2] {
+			return true
+		}
+	}
+	return false
 }
