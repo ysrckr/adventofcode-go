27140ecabd2b94a35a5540b96f0c1c4679ee99ed
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-08 08:01:16 +0700

    Add grid creation and file opening functions

--- 2015/day5/puzzle2/main.go
+++ 2015/day5/puzzle2/main.go
@@ -69,3 +69,25 @@ func hasRepeatedWithOneBetween(s string) bool {
 	}
 	return false
 }
+
+func createGrid(fill int) [1000][1000]int {
+	grid := [1000][1000]int{}
+	for i := 0; i < 1000; i++ {
+		for j := 0; j < 1000; j++ {
+			grid[i][j] = fill
+		}
+	}
+
+	return grid
+}
+
+func openFile() *bufio.Reader {
+	file, err := os.Open("../input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	reader := bufio.NewReader(file)
+	return reader
+}
+
