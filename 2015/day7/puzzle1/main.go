f5f223b8b3f8b818da05b51e4786b7b276f0768f
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-12 15:31:07 +0700

    <commit message>Add operate function to handle logic operations

--- 2015/day7/puzzle1/main.go
+++ 2015/day7/puzzle1/main.go
@@ -114,6 +114,63 @@ func rightShiftOperation(a, b int) int {
 	return a >> b
 }
 
+func operate(line *string, values map[string]int) {
+	re := regexp.MustCompile(`(\w{1,2}) (LSHIFT|RSHIFT|AND|OR) (\w{1,2}|\d) -> (\w{1,2})`)
+	renot := regexp.MustCompile(`(NOT) (\w{1,2}|\d) -> (\w{1,2})`)
+	reg := regexp.MustCompile(`(\w{1,2}) -> (\w{1,2})`)
+
+	matchSwap := reg.MatchString(*line)
+	if matchSwap == true {
+		match := reg.FindStringSubmatch(*line)
+
+		values[match[2]] = values[match[1]]
+	}
+
+	match := re.MatchString(*line)
+	if match == true {
+		match := re.FindStringSubmatch(*line)
+		switch match[2] {
+		case "AND":
+			a := values[match[1]]
+			b := values[match[3]]
+
+			result := andOperation(a, b)
+
+			values[match[4]] = result
+		case "OR":
+			a := values[match[1]]
+			b := values[match[3]]
+
+			result := orOperation(a, b)
+
+			values[match[4]] = result
+		case "LSHIFT":
+			a := values[match[1]]
+			b := values[match[3]]
+
+			result := leftShiftOperation(a, b)
+
+			values[match[4]] = result
+
+		case "RSHIFT":
+			a := values[match[1]]
+			b := values[match[3]]
+
+			result := rightShiftOperation(a, b)
+
+			values[match[4]] = result
+		}
+	}
+	matchnot := renot.MatchString(*line)
+	if matchnot == true {
+		match := renot.FindStringSubmatch(*line)
+		a := values[match[2]]
+		result := notOperation(a)
+		values[match[3]] = result
+	}
+
+}
+
 func puzzle1() int {
 	file := openFile()
 
@@ -123,7 +180,13 @@ func puzzle1() int {
 	values := findValues(&lines)
 	filterCircuit(&lines)
 
-	fmt.Println(values)
-	fmt.Println(lines)
-	return 0
+	fmt.Println("before: ", values)
+
+	for _, line := range lines {
+		operate(&line, values)
+	}
+
+	fmt.Println("after: ", values)
+
+	return values["a"]
 }
