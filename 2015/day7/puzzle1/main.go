cf71532fd0f0a43c1c38a3a419163fe13ff4037d
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-12 22:01:37 +0700

    Refactor puzzle1 by removing unused functions

--- 2015/day7/puzzle1/main.go
+++ 2015/day7/puzzle1/main.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"regexp"
 	"strconv"
 )
 
@@ -67,27 +66,10 @@ func readLines(reader *bufio.Reader) []string {
 	return lines
 }
 
-func findValues(lines *[]string) map[string]uint16 {
-	values := map[string]uint16{}
-	re := regexp.MustCompile(`(\d+) -> (\w+)`)
+func puzzle1(lines []string) uint16 {
+	var wires map[string]string
 
-	for _, line := range *lines {
-		match := re.FindStringSubmatch(line)
-		if len(match) == 0 {
-			continue
-		}
-
-		matchInInt, err := strconv.Atoi(match[1])
-		if err != nil {
-			continue
-		}
-
-		values[match[2]] = uint16(matchInInt)
-	}
-
-	fmt.Println(values)
-
-	return values
+	return 0
 }
 
 func notOperation(a uint16) uint16 {
@@ -110,104 +92,10 @@ func rightShiftOperation(a, b uint16) uint16 {
 	return a >> b
 }
 
-func operate(line *string, values map[string]uint16) {
-	re := regexp.MustCompile(`(\w{1,2}) (RSHIFT|AND|OR|LSHIFT) (\w{1,2}|\d) -> (\w{1,2})`)
-	renot := regexp.MustCompile(`(NOT) (\w{1,2}|\d) -> (\w{1,2})`)
-	reg := regexp.MustCompile(`(\w{1,2}) -> (\w{1,2})`)
-
-	matchreg := reg.MatchString(*line)
-	if matchreg == true {
-		match := reg.FindStringSubmatch(*line)
-		var v uint16
-		r, err := strconv.Atoi(match[1])
-		if err != nil {
-			v = values[match[1]]
-		}
-
-		v = uint16(r)
-
-		values[match[2]] = v
+func isInt(possibleInt string) bool {
+	if _, err := strconv.Atoi(possibleInt); err != nil {
+		return false
 	}
 
-	matchnot := renot.MatchString(*line)
-	if matchnot == true {
-		match := renot.FindStringSubmatch(*line)
-		a := values[match[2]]
-		result := notOperation(a)
-
-		values[match[3]] = result
-	}
-
-	match := re.MatchString(*line)
-	if match == true {
-		match := re.FindStringSubmatch(*line)
-		switch match[2] {
-		case "AND":
-			a := values[match[1]]
-
-			var b uint16
-			r, err := strconv.Atoi(match[3])
-			if err != nil {
-				b = values[match[3]]
-			}
-
-			b = uint16(r)
-
-			result := andOperation(a, b)
-
-			values[match[4]] = result
-		case "OR":
-			a := values[match[1]]
-			var b uint16
-			r, err := strconv.Atoi(match[3])
-			if err != nil {
-				b = values[match[3]]
-			}
-
-			b = uint16(r)
-
-			result := orOperation(a, b)
-
-			values[match[4]] = result
-		case "LSHIFT":
-			a := values[match[1]]
-			var b uint16
-			r, err := strconv.Atoi(match[3])
-			if err != nil {
-				b = values[match[3]]
-			}
-
-			b = uint16(r)
-
-			result := leftShiftOperation(a, b)
-
-			values[match[4]] = result
-
-		case "RSHIFT":
-			a := values[match[1]]
-			var b uint16
-			r, err := strconv.Atoi(match[3])
-			if err != nil {
-				b = values[match[3]]
-			}
-
-			b = uint16(r)
-
-			result := rightShiftOperation(a, b)
-
-			values[match[4]] = result
-		}
-	}
-
-}
-
-func puzzle1(lines []string) uint16 {
-
-	values := findValues(&lines)
-
-	for _, line := range lines {
-		operate(&line, values)
-	}
-
-	return values["a"]
+	return true
 }
